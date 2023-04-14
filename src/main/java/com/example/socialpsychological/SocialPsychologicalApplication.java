f40acd17139c9ce6a72bbc2be1ffaf1bde55426b
Bùi Xuân Hiếu <bhieu713@gmail.com>
2023-04-14 16:13:12 +0700

    update v2.0

---
+++ src/main/java/com/example/socialpsychological/SocialPsychologicalApplication.java
@@ -0,0 +1,13 @@
+package com.example.socialpsychological;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialPsychologicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialPsychologicalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/socialpsychological/SocialPsychologicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socialpsychological;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialPsychologicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
